3e38f82cd8efe6c7f4188e64a0adb8d3ce8f373e
Tetiana Sobchyshyna <43253717+TetianaSob@users.noreply.github.com>
2020-12-05 19:29:04 -0500

    modules_import_math

---
+++ modules_import_math.py
@@ -0,0 +1,26 @@
+#modules_import_math.py
+
+import life
+
+type(life)
+#module
+theAnswer
+life.theAnswer
+#42
+import math
+math.pi
+#3.141592653589793
+#e
+math.e
+#2.718281828459045
+from life import theAnswer
+    print(myFavouriteNumber)
+myFavouriteNumber = theAnswer
+    print(myFavouriteNumber)
+#42
+theAnswer = theAnswer + 1
+    print(theAnswer)
+theAnswer = theAnswer + 1
+    print(theAnswer)
+#43
+​
